6ecaa4698fb10a73beda0ca89ec48538e58c0efe
Christopher Dougherty <christophertdoc@gmail>
2020-06-22 21:58:03 -0400

    add query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 
 class SongList extends Component {
 
@@ -8,5 +9,13 @@ class SongList extends Component {
     );
   }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
 
 export default SongList;
